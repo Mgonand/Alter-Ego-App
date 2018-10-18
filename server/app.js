72a39098c3144041fff8c103326f2fab937e6060
cesararmada <cesar.armada@gmail.com>
2018-10-18 18:22:22 +0200

    heroku

--- server/app.js
+++ server/app.js
@@ -68,7 +68,7 @@ app.use("/api/games",chapterRoutes)
 
 app.use((req, res, next) => {
   // If no routes match, send them the React HTML.
-  res.sendFile(path.join(__dirname + "/public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 module.exports = app;  
